9ed713f13e7346ff1feee6dcdd67c4c5ebf9ee65
mxl2 <marian.lumba@nreca.coop>
2015-12-02 16:37:33 -0500

    media view bootstrap, change constants, remove bloat from previous states

--- dist/js/app.js
+++ dist/js/app.js
@@ -105,154 +105,35 @@
         pageTitle: 'Review'
       }
     });
-    /*$stateProvider.state('othertransportation', {
-      url: '/othertransportation',
-      views: {
-        '' : {
-          templateUrl: '/templates/othertransportation.html',
-          controller: 'Main.controller',
-        },
-        'navigation@othertransportation' : {
-          templateUrl: '/templates/navigation.html'
-        }
-      },
-      data: {
-        page: 7,
-        pageTitle: 'Other Transportation'
-      }
-    });
-    $stateProvider.state('parking', {
-      url: '/parking',
-      views: {
-        '' : {
-          templateUrl: '/templates/parking.html',
-          controller: 'Main.controller',
-        },
-        'navigation@parking' : {
-          templateUrl: '/templates/navigation.html'
-        }
-      },
-      data: {
-        page: 8,
-        pageTitle: 'Parking'
-      }
-    });
-    $stateProvider.state('hotel', {
-      url: '/hotel',
-      views: {
-        '' : {
-          templateUrl: '/templates/hotel.html',
-          controller: 'Main.controller',
-        },
-        'navigation@hotel' : {
-          templateUrl: '/templates/navigation.html'
-        }
-      },
-      data: {
-        page: 9,
-        pageTitle: 'Other Transportation'
-      }
-    });
-    $stateProvider.state('otherexpenses', {
-      url: '/otherexpenses',
-      views: {
-        '' : {
-          templateUrl: '/templates/otherexpenses.html',
-          controller: 'Main.controller',
-        },
-        'navigation@otherexpenses' : {
-          templateUrl: '/templates/navigation.html'
-        }
-      },
-      data: {
-        page: 10,
-        pageTitle: 'Other Expenses'
-      }
-    });
-    $stateProvider.state('deductions', {
-      url: '/deductions',
-      views: {
-        '' : {
-          templateUrl: '/templates/deductions.html',
-          controller: 'Main.controller',
-        },
-        'navigation@deductions' : {
-          templateUrl: '/templates/navigation.html'
-        }
-      },
-      data: {
-        page: 11,
-        pageTitle: 'Deductions'
-      }
-    });
-    $stateProvider.state('perdiem', {
-      url: '/perdiem',
-      views: {
-        '' : {
-          templateUrl: '/templates/perdiem.html',
-          controller: 'Main.controller',
-        },
-        'navigation@perdiem' : {
-          templateUrl: '/templates/navigation.html'
-        }
-      },
-      data: {
-        page: 12,
-        pageTitle: 'Per Diem'
-      }
-    });
-    $stateProvider.state('receipts', {
-      url: '/receipts',
-      views: {
-        '' : {
-          templateUrl: '/templates/receipts.html',
-          controller: 'Main.controller',
-        },
-        'navigation@receipts' : {
-          templateUrl: '/templates/navigation.html'
-        }
-      },
-      data: {
-        page: 13,
-        pageTitle: 'Receipts'
-      }
-    });
-    $stateProvider.state('review', {
-      url: '/review',
-      views: {
-        '' : {
-          templateUrl: '/templates/review.html',
-          controller: 'Main.controller',
-        },
-        'navigation@review' : {
-          templateUrl: '/templates/navigation.html'
-        }
-      },
-      data: {
-        page: 14,
-        pageTitle: 'Review'
-      }
-    });
-    $stateProvider.state('pageIndex', {
-        url: '/page/:index',
-        controller: function($state, $stateParams, pages) {
-          var state = pages[$stateParams.index];
-          if ( state != null ) {
-            $state.go(state)
-          }
-        }
-    });*/
   }]);
 
-  app.controller("Main.controller", ['$scope', '$state', function($scope, $state){
+  app.constant('pages', [
+    'home',
+    'general',
+    'travel',
+    'expenses',
+    'deductions',
+    'review'
+  ]);
+
+  app.controller("Main.controller", ['$scope', '$state', 'pages', function($scope, $state, pages){
     var myState = $state.$current.data.page;
-    console.log(myState);
+    var stateName = $state.$current.name;
+    /*for (var st in $state.get()){
+      console.log(stateName);
+    }*/
+    console.log("Total pages: " + pages.length);
+    //console.log(angular.toJson($state.get()));
+    console.log("current state: " + myState);
+    console.log("current state name: " + stateName);
+    var currentStateNum = pages.indexOf(stateName);
+    console.log("next state " + (currentStateNum + 1))
     $scope.currentPageTitle = $state.$current.data.pageTitle;
     $scope.goNext = function goNext() {
-      $state.go('pageIndex', { index: myState });
+      $state.go('myState', { index: myState + 1});
     };
     $scope.goPrevious = function goPrevious() {
-      $state.go('pageIndex', { index: myState - 1});
+      $state.go('myState', { index: myState - 1});
     };
     $scope.expense = {
       availableOptions: [
@@ -278,34 +159,8 @@
     }
   }]);
 
-  app.constant('pages', [
-    'home',
-    'general',
-    'travel',
-    'purpose',
-    'airfare',
-    'car',
-    'othertransportation',
-    'parking',
-    'hotel',
-    'otherexpenses',
-    'deductions',
-    'perdiem',
-    'receipts',
-    'review'
-  ]);
-
   module.exports = app;
 })();
-
-/*
-  /page/:index (where index >= 1)
-    > /page/14 == look at service for next state, redirect to state, index in service is :index - 1
-  service = [
-    'review',
-    'receipts'
-  ];
-*/
 
 },{}],2:[function(require,module,exports){
 var app = require('./app.js');
